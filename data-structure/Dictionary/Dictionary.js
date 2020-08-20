db5a369d49623e0a60eda5840c6d5e0ddd0e45c2
7orz <ShieTsun@outlook.com>
2020-08-20 16:38:35 +0800

    安装browser-sync

--- data-structure/Dictionary/Dictionary.js
+++ data-structure/Dictionary/Dictionary.js
@@ -1,3 +1,37 @@
-export default class Dictionary{
-    
+import {
+    defaultToString as toStrFn,
+    ValuePair
+} from '../util.js'
+
+const mountDiv = document.getElementById("mount")
+
+export default class Dictionary {
+    constructor(toStr = toStrFn) {
+        this.toStr = toStr
+        this.table = {}
+    }
+    has(key) {
+        return this.table[this.toStr(key)] != null
+    }
+    set(key, value) {
+        if (key != null && value != null) {
+            const tableKey = this.toStr(key)
+            this.table[tableKey] = new ValuePair(key, value)
+            return true
+        }
+        return false
+    }
+    get(key) {
+        const valuePair = this.table[this.toStr(key)]
+        valuePair == null ? undefined : valuePair.value
+    }
+    del(key) {
+        if (this.has(key)) {
+            delete this.table[this.toStr(key)]
+            return true
+        }
+        return false
+    }
 }
+
+mountDiv.innerHTML = `${toStrFn}`
